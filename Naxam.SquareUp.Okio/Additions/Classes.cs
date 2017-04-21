3264223e832dc235f4d21cb6f7e8fbd469cd28c2
Vu Duc Tuyen <tuyen@naxam.net>
2017-04-21 23:10:39 +0700

    Square Okio 1.12.0

---
+++ Naxam.SquareUp.Okio/Additions/Classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Java.Lang;
+
+namespace Okio
+{
+    public partial class ByteString
+    {
+        public int CompareTo(Java.Lang.Object obj) {
+            return CompareTo(obj as ByteString);
+        }
+    }
+
+    public partial class Options {
+        public override Java.Lang.Object Get(int index) {
+            return GetByteString(index);
+        }
+    }
+}
