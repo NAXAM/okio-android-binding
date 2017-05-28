b072f99adc4e1d3c4b85221f6bc69d4a8e48f879
Vu Duc Tuyen <tuyen@naxam.net>
2017-05-28 23:50:47 +0700

    Okio v1.13.0

--- Naxam.SquareUp.Okio/Properties/AssemblyInfo.cs
+++ Naxam.SquareUp.Okio/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using Android.App;
 [assembly: AssemblyTitle("Naxam.SquareUp.Okio")]
 [assembly: AssemblyDescription("Xamarin Android Binding Library for Square Okio")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NAXAM CO.,LTD")]
-[assembly: AssemblyProduct("Xam Binding")]
+[assembly: AssemblyCompany("NAXAM COMPANY LIMITED")]
+[assembly: AssemblyProduct("X Bindings")]
 [assembly: AssemblyCopyright("(c) 2017 NAXAM")]
 [assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
@@ -18,8 +18,8 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.12.0")]
-[assembly: AssemblyInformationalVersion("1.12.0")]
+[assembly: AssemblyVersion("1.13.0")]
+[assembly: AssemblyInformationalVersion("1.13.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
